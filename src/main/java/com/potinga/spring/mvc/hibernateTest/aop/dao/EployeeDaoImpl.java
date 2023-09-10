6068d72bdf2d9b42f6c1a03061d4f26f9a849793
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-09-10 21:39:39 +0300

    Create EployeeDaoImpl.java

---
+++ src/main/java/com/potinga/spring/mvc/hibernateTest/aop/dao/EployeeDaoImpl.java
@@ -0,0 +1,23 @@
+package com.potinga.spring.mvc.hibernateTest.aop.dao;
+
+import com.potinga.spring.mvc.hibernateTest.aop.entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+import java.util.List;
+@Repository
+public class EployeeDaoImpl implements EmployeeDAO {
+    @Autowired
+    private SessionFactory sessionFactory;
+    @Override
+    @Transactional
+    public List<Employee> getAllEmployee() {
+        Session session = sessionFactory.getCurrentSession();
+     List<Employee>allEmployees = session.createQuery("from Employee",
+             Employee.class).getResultList();
+        return allEmployees ;
+    }
+}
