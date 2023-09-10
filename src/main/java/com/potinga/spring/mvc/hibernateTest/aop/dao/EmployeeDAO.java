30c4d3b8d0963c424c7ca28264f0cb4785e6885d
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-09-10 21:39:34 +0300

    Create EmployeeDAO.java

---
+++ src/main/java/com/potinga/spring/mvc/hibernateTest/aop/dao/EmployeeDAO.java
@@ -0,0 +1,10 @@
+package com.potinga.spring.mvc.hibernateTest.aop.dao;
+
+import com.potinga.spring.mvc.hibernateTest.aop.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeDAO {
+    public List<Employee>getAllEmployee();
+
+}
