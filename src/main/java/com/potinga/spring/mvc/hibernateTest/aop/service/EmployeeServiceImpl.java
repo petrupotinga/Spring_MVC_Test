3b1f91fa40b5849a7b7179ff868aa149e38b29ea
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-09-11 15:13:46 +0300

    Create EmployeeServiceImpl.java

---
+++ src/main/java/com/potinga/spring/mvc/hibernateTest/aop/service/EmployeeServiceImpl.java
@@ -0,0 +1,21 @@
+package com.potinga.spring.mvc.hibernateTest.aop.service;
+
+import com.potinga.spring.mvc.hibernateTest.aop.dao.EmployeeDAO;
+import com.potinga.spring.mvc.hibernateTest.aop.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+    @Autowired
+    private EmployeeDAO employeeDAO;
+
+    @Override
+    @Transactional
+    public List<Employee> getAllEmployees() {
+        return employeeDAO.getAllEmployees();
+    }
+}
