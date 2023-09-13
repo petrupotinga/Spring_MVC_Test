4f7f21a1a43390c72f7cb298dc621c2cb6696b91
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-09-13 21:00:01 +0300

    Update MyController.java

--- src/main/java/com/potinga/spring/mvc/hibernateTest/aop/controller/MyController.java
+++ src/main/java/com/potinga/spring/mvc/hibernateTest/aop/controller/MyController.java
@@ -6,6 +6,7 @@ import com.potinga.spring.mvc.hibernateTest.aop.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -20,5 +21,18 @@ public class MyController {
         List<Employee> allEmployees = employeeService.getAllEmployees();
         model.addAttribute("allEmps", allEmployees);
         return "all-employees";
+    }
+
+    @RequestMapping("/addNewEmployee")
+    public String addNewEmployee(Model model) {
+        Employee employee = new Employee();
+        model.addAttribute("employee", employee);
+        return "employee-info";
+    }
+
+    @RequestMapping("/saveEmployee")
+    public String saveEmployee(@ModelAttribute("employee") Employee employee) {
+        employeeService.saveEmployee(employee);
+        return "redirect:/";
     }
 }
