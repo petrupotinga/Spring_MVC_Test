e58e6864e5e99eaaab78b823dc081b8011bfe80e
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-09-14 20:13:11 +0300

    Update Employee.java

--- src/main/java/com/potinga/spring/mvc/hibernateTest/aop/entity/Employee.java
+++ src/main/java/com/potinga/spring/mvc/hibernateTest/aop/entity/Employee.java
@@ -28,6 +28,14 @@ public class Employee {
         this.salary = salary;
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
     }
