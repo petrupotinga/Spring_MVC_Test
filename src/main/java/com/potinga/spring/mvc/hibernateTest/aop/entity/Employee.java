990148e5c49730d5ff1af29761a462b831b5cdb3
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-09-14 12:40:24 +0300

    Update Employee.java

--- src/main/java/com/potinga/spring/mvc/hibernateTest/aop/entity/Employee.java
+++ src/main/java/com/potinga/spring/mvc/hibernateTest/aop/entity/Employee.java
@@ -7,6 +7,8 @@ import javax.persistence.*;
 public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
     @Column(name = "name")
     private String name;
     @Column(name = "surname")
