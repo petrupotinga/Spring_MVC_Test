abf20774f43fadbebff6e201ca1059908a8f076a
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-09-17 20:41:14 +0300

    Update MyController.java

--- src/main/java/com/potinga/spring/mvc/hibernateTest/aop/controller/MyController.java
+++ src/main/java/com/potinga/spring/mvc/hibernateTest/aop/controller/MyController.java
@@ -43,6 +43,11 @@ public class MyController {
         model.addAttribute("employee", employee);
 
         return "employee-info";
+    }
+    @RequestMapping("/deleteEmployee")
+    public String deleteEmployee(@RequestParam("empId")int id){
+       employeeService.deleteEmployee(id);
+        return "redirect:/";
 
     }
 }
