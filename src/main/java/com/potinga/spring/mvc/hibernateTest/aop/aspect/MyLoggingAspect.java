41aa4af046600a578b3590d216f36e271f422458
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-09-19 18:01:15 +0300

    Update MyLoggingAspect.java

--- src/main/java/com/potinga/spring/mvc/hibernateTest/aop/aspect/MyLoggingAspect.java
+++ src/main/java/com/potinga/spring/mvc/hibernateTest/aop/aspect/MyLoggingAspect.java
@@ -1,9 +1,25 @@
 package com.potinga.spring.mvc.hibernateTest.aop.aspect;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
 
+import java.lang.reflect.Method;
+
+@Component
+@Aspect
 public class MyLoggingAspect {
-
-    public Object aroundAllRepositoryMethodsAdvice(){
-
+    @Around("execution(* com.potinga.spring.mvc.hibernateTest.aop.dao.*.*(..))")
+    public Object aroundAllRepositoryMethodsAdvice(
+            ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        MethodSignature methodSignature =
+                (MethodSignature) proceedingJoinPoint.getSignature();
+        String methodName = methodSignature.getName();
+        System.out.println("Begin off" + methodName);
+        Object targetMethodResult = proceedingJoinPoint.proceed();
+        System.out.println("End off" + methodName);
+        return targetMethodResult;
     }
 }
